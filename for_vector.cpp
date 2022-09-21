14f3f7e3cc1475c79bca237cf693360c9076ac2f
jewel <jewelnmaldo@gmail.com>
2022-09-21 09:48:11 -0800

    testing for loop

---
+++ for_vector.cpp
@@ -0,0 +1,27 @@
+/**
+ * Sample program for demonstrating for an vector
+ *
+ * @authro Jewel Maldonado
+ * @date Sep 21 2022
+ * @file for_vector
+ * @brief demostrate use of for and vector
+ */
+
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+	
+	for ( int i = 0; i < 5; i++ ){
+		cout << i << endl;
+	}
+
+	return 0;
+}
